9fb71330c533968c0b085daa1c4875e1bc4ed7a1
Nawshin Sharmily <nsharmily11@gmail.com>
2026-01-07 04:33:50 +0000

    build safe

---
+++ src/types/index.ts
@@ -0,0 +1,77 @@
+import { Models } from "appwrite";
+
+export type INavLink = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+
+export type INewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
+
+export type ISaveDocument = Models.Document & {
+  user: IUserDocument;
+  post: IPostDocument;
+};
+
+export type IPostDocument = Models.Document & {
+  creator: IUserDocument;
+  caption: string;
+  imageUrl: string;
+  imageId: string;
+  location: string;
+  tags: string[];
+  likes: IUserDocument[];
+};
+
+export type IUserDocument = Models.Document & {
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  imageId: string;
+  bio: string;
+  posts: IPostDocument[];
+  liked: IPostDocument[];
+  save: ISaveDocument[];
+};
